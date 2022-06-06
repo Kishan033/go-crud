57ca89a729f6a3ff6ba3c0c18b50e18d02cc7683
Kishan Dobariya <kishan@jungleeegames.com>
2022-06-06 17:39:32 +0530

    test commit

--- db/connection.go
+++ db/connection.go
@@ -3,7 +3,6 @@ package db
 import (
 	"database/sql"
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -14,20 +13,20 @@ func InnitializeDBService() *sql.DB {
 	err := godotenv.Load(".env")
 
 	if err != nil {
-		log.Fatalf("Error loading .env file")
+		// log.Fatalf("Error loading .env file")
 	}
 
 	dbcon, err := sql.Open("postgres", os.Getenv("POSTGRES_URL"))
 
 	if err != nil {
-		panic(err)
+		// panic(err)
 	}
 
 	// check the connection
 	err = dbcon.Ping()
 
 	if err != nil {
-		panic(err)
+		// panic(err)
 	}
 
 	fmt.Println("Successfully connected to DB!")
